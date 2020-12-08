fc570d958f060df42e3913598c3a085ffab80dad
Chigbogu Orji <brightorji60@gmail.com>
2020-12-08 21:46:08 +0100

    Fix: set proper word boundary

--- src/ParsedText.jsx
+++ src/ParsedText.jsx
@@ -4,7 +4,7 @@ const ParsedText = ({value='#### Some markdown text'}) => {
   return (
     <div className='h-100'>
       <h4 className='text-danger'><u>Preview</u></h4>
-      <div dangerouslySetInnerHTML={{__html: marked(value)}}>
+      <div dangerouslySetInnerHTML={{__html: marked(value)}} style={{wordWrap: 'break-word'}}>
       </div>
     </div>
   )
